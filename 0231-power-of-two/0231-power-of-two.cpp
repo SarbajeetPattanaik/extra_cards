b54bd47a8e7d1a96a9344f28059a198e8da4ee7b
Sarbajeet Pattanaik <157008284+SarbajeetPattanaik@users.noreply.github.com>
2024-03-31 22:38:28 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (76.97%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if (n == 1) return true;
-        else if (n <= 0) return false;
+        if (n <= 0) return false;
+        else if (n == 1) return true;
         else if (n % 2 != 0) return false;
         return isPowerOfTwo(n/2);
     }
