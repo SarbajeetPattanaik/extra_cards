ae23ad641593500146b609f4b3aaeb9af74a0cf4
Sarbajeet Pattanaik <157008284+SarbajeetPattanaik@users.noreply.github.com>
2024-07-23 19:26:38 +0530

    Time: 7 ms (55.39%), Space: 14.8 MB (70.42%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -31,9 +31,17 @@ public:
         
         vector<int> results;
         
-        for(auto it:temp){
-            for(int i=0;i<it.second;i++){
-            results.push_back(it.first);}
+        // for(auto it:temp){
+        //     for(int i=0;i<it.second;i++){
+        //     results.push_back(it.first);}
+        // }
+        
+        
+        vector<pair<int,int>>::iterator v_it=temp.begin();
+        
+        for(v_it=temp.begin();v_it!=temp.end();v_it++){
+            for(int i=0;i<v_it->second;i++){
+            results.push_back(v_it->first);}
         }
         return results;
         
