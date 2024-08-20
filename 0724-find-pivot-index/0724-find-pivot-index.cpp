8e0305bb1825dda06797deafd5a728e012aaab8f
Sarbajeet Pattanaik <157008284+SarbajeetPattanaik@users.noreply.github.com>
2024-08-21 00:08:17 +0530

    Time: 24 ms (22.33%), Space: 34.7 MB (51.83%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        // vector<int> pf;
+        int ls=0,sum1=0;       // vector<int> sf(nums.size()-1,0);
+        sum1=accumulate(nums.begin(),nums.end(),0);
+
+        for( int i=0;i<nums.size();i++){
+            if(ls==sum1-ls-nums[i]){
+                return i;
+            }
+
+            ls=ls+nums[i];
+        }
+
+        return -1;
+
+        
+    }
+};
